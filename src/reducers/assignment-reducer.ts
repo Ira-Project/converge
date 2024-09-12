cf1acbe1725dbbf40452831c5f8f71530ee14101
Vignesh Prasad <vigfb9295@gmail.com>
2024-09-13 04:05:44 +0530

    minor fix to assignment reducer

--- src/reducers/assignment-reducer.ts
+++ src/reducers/assignment-reducer.ts
@@ -15,7 +15,7 @@ export const questionReducer = (
           working: '',
           computedAnswerText: "",
           workingComplete: false,
-          image: undefined,
+          image: "",
         };
       });
       return {
