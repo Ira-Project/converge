fb0a75048decc8b6ddbd9948ea633db18e5f81fe
Vignesh Prasad <vigfb9295@gmail.com>
2025-01-13 23:06:09 +0530

    redirect for default pages

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,18 @@
+import { validateRequest } from "@/lib/auth/validate-request";
+import { Paths } from "@/lib/constants";
+import { redirect } from "next/navigation";
+
  export default async function Home({}) {
+
+  const { user } = await validateRequest();
+
+  if(!user) redirect(Paths.Login);
+
+  if(user.classroomId) {
+    redirect(`${Paths.Classroom}${user.classroomId}`);
+  } else {
+    redirect(Paths.Onboarding);
+  }
   
   return (
     <>
