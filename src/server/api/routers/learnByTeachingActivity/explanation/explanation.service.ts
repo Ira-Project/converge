43109e37f50764568b7d0ac99eb1967c229a79d0
Nayak <likhitnayak@gmail.com>
2025-02-25 02:09:51 +0530

    ignoring questions in learn_by_teach

--- src/server/api/routers/learnByTeachingActivity/explanation/explanation.service.ts
+++ src/server/api/routers/learnByTeachingActivity/explanation/explanation.service.ts
@@ -105,6 +105,9 @@ export const explain = async (ctx: ProtectedTRPCContext, input: ExplainInput) =>
 
 
   for(const [index, question] of questionList.entries()) {
+    // if (index !== 0) {
+    //   continue;
+    // }
     const fetchUrl = `${process.env.BASE_REASONING_ENGINE_URL}${question.lambdaUrl}`;
     const response = fetch(fetchUrl, {
       method: "POST",
