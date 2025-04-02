424b2fde0a3d3fb98f724e6cffb56ae37a7db39a
Vignesh Prasad <vigfb9295@gmail.com>
2025-04-01 20:12:41 -0700

    added posthog page load capture

--- next.config.js
+++ next.config.js
@@ -5,23 +5,41 @@ const config = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'source.boringavatars.com',
+        protocol: "https",
+        hostname: "source.boringavatars.com",
       },
       {
-        protocol: 'https',
-        hostname: 'converge-ira-project.s3.ap-south-1.amazonaws.com'
+        protocol: "https",
+        hostname: "converge-ira-project.s3.ap-south-1.amazonaws.com",
       },
       {
-        protocol: 'https',
-        hostname: 'converge-question-images.s3.us-west-1.amazonaws.com'
-      }
+        protocol: "https",
+        hostname: "converge-question-images.s3.us-west-1.amazonaws.com",
+      },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default config;
